ec1121ffaed9ef16239bd6f85d9cad2733b440eb
Kadir Goksal <136205319+KGoksal@users.noreply.github.com>
2024-07-26 09:04:03 -0500

    Update init.sql

--- python/init.sql
+++ python/init.sql
@@ -1,14 +1,14 @@
-CREATE TABLE phonebook_db.phonebook(
+CREATE TABLE phonebook_db.phonebook (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(100) NOT NULL,
     number VARCHAR(100) NOT NULL,
     PRIMARY KEY (id)
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 INSERT INTO phonebook_db.phonebook (name, number)
-    VALUES
-        ("Mike Ehrmantraut", "1234567890"),
-        ("Walter White Jr.", "69827854"),
-        ("Jesse Pinkman", "876543554");
-        ("Gustavo Fring", "987543554");
-        ("Hector Salamanca", "9876543234");
+VALUES
+    ('Mike Ehrmantraut', '1234567890'),
+    ('Walter White Jr.', '69827854'),
+    ('Jesse Pinkman', '876543554'),
+    ('Gustavo Fring', '987543554'),
+    ('Hector Salamanca', '9876543234');
